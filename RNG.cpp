cb0b71f1d9e1679a6136e180b2c613424328187c
NotSoToothless <67082114+FURRO404@users.noreply.github.com>
2022-07-23 02:35:27 -0700

    Statistics section

--- RNG.cpp
+++ RNG.cpp
@@ -8,21 +8,30 @@ int main()
     int amount;
     int lowest;
     int highest;
+    int keep_going_lol = 1;
 
-    cout << "How many numbers: ";
-    cin >> amount;
+    while (keep_going_lol == 1){
+        cout << "How many numbers: ";
+        cin >> amount;
 
-    cout << "\nLowest Number: ";
-    cin >> lowest;
+        cout << "\nLowest Number: ";
+        cin >> lowest;
 
-    cout << "\nHighest Number: ";
-    cin >> highest;
+        cout << "\nHighest Number: ";
+        cin >> highest;
 
-    for (int i = 0; i < amount; i++){
-        random_number = rand() % (highest-lowest) + lowest;   // Number in the range lowest - highest
-        cout << i << ": " << random_number << endl;
+        int avg = 0;
+        for (int i = 1; i < amount + 1; i++){
+            random_number = rand() % (highest-lowest) + lowest;   // Number in the range lowest - highest
+            cout << i << ": " << random_number << endl;
+
+            //statistic gathering
+            avg = random_number + avg;
+        }
+
+        avg = avg/amount;
+        cout << "\n\nAverage: " << avg << endl;
     }
-
     return 0;
 }
 /*RNG.cpp by FURRO404*/
